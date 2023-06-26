fa7927f184eb3f2e967ac3c8043b13ed6b4f117c
abel-Vamverse <abel@vamverse.com>
2023-06-26 16:31:47 +0100

    new name

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 const Home = () => {
-  return <Box>HELLO THERE TESTED SUCCESSFULLY BY SAURABH</Box>
+  return <Box>HELLO THERE TESTED SUCCESSFULLY BY Abel</Box>
 }
 
 export default Home
