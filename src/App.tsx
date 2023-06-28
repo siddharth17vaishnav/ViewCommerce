b347b80290dc5558ad87a9418a6d747ee9d8cb06
usaurabh02 <73464543+usaurabh02@users.noreply.github.com>
2023-06-28 20:21:31 +0530

    Revert "Footer created"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './sections/Home'
-import Footer from './components/Footer/Footer'
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer/>
     </>
   )
 }
