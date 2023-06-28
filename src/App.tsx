2c8842702a1f081f63f364cc666626d11fbbb4ee
Ashishvam <ashish@vamverse.com>
2023-06-28 20:21:55 +0530

    Card compoenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './sections/Home'
 import Footer from './components/Footer/Footer'
+import Card from './components/Card/Card'
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+     <Card />
       <Footer/>
     </>
   )
