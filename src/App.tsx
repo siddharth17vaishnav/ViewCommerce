40a95d90b1762e9a4e7fbb7f294d5d4bd14df756
Ashishvam <ashish@vamverse.com>
2023-06-30 11:36:48 +0530

    Footer updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './sections/Home'
 import Footer from './components/Footer/Footer'
-import Card from './components/Card/Card'
-
 function App() {
   return (
     <>
-
-
-
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-     <Card />
       <Footer/>
-
-
-      
     </>
   )
 }
