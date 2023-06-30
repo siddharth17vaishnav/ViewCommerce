b5f0b4e57dab63fc8783abded249e28b45ccfc87
belayhun-vamverse <belayhun@vamverse.com>
2023-06-30 12:15:30 +0300

    working on fixing BestDealCards and SmartphonePhoneCards

--- src/components/PhoneCard/index.tsx
+++ src/components/PhoneCard/index.tsx
@@ -1,36 +1,40 @@
+import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import IPhone from '../../assets/iPhone.jpg'
-
-const PhoneCard=()=> {
+import { Button, CardActionArea, CardActions, CardHeader,IconButton } from '@mui/material';
+import Grid from '@mui/material';
+const PhoneCard=({item})=> {
   return (
-    <div style={{margin: '25%'}}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={IPhone}
-        alt="Chevrolet"
+      <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        action={
+          <IconButton>
+            <svg width="65" height="67" viewBox="0 0 65 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.427979 0.257202H44.0631C55.0798 0.257202 64.0107 9.188 64.0107 20.2047V66.3333H20.3755C9.35879 66.3333 0.427979 57.4025 0.427979 46.3858V0.257202Z" fill="#008ECC"/>
+          50% OFF
+          </svg>
+          </IconButton>
+        }
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          colors
+      <CardActionArea>
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div" >
+          <img src={item.image} />
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Wood lamp, Floor Lamp, Lambader, Decorative
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        458,43 $
-        </Typography>
-      </CardContent>
-      
+          <Typography variant="body2" color="text.secondary">
+            {item.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+         Save {item.price}
+        </Button>
+      </CardActions>
     </Card>
-    </div>
   );
 }
 
-export default PhoneCard;
+export default PhoneCard
--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,21 +1,45 @@
 import { Box, Grid } from '@mui/material'
 import MediaCard from '../../components/BestDeal'
-import PhoneCard from '@/components/PhoneCard'
-import IPhone from '../../assets/iPhone.jpg'
+import PhoneCard from '../../components/PhoneCard'
+import Instrument from '../../assets/Music.jpg'
+import IPhone from '../../assets/Phone.jpg'
+import TRAS from '../../assets/TRAS.jpg'
+import Image3 from '../../assets/image 3.jpg'
 import Footer from '../../components/common/Footer'
 const Home = () => {
   const itemList = [
-    { id: 1, title: 'Item 1', description: 'Description for Item 1',price:45843,image:IPhone },
+    { id: 1, title: 'Item 1',bgColor:'#FFE4BD', description: 'Description for Item 1',price:45843,image:Instrument },
+    { id: 2, title: 'Item 2',bgColor:'#FFBDBD', description: 'Description for Item 2',price:45843,image:IPhone },
+    { id: 3, title: 'Item 3',bgColor:'#FDF0AB', description: 'Description for Item 3',price:45843,image:TRAS },
+  ];
+
+  const phoneItemList = [
+    { id: 1, title: 'Item 1', description: 'Description for Item 1',price:45843,image:Image3 },
     { id: 2, title: 'Item 2', description: 'Description for Item 2',price:45843,image:IPhone },
     { id: 3, title: 'Item 3', description: 'Description for Item 3',price:45843,image:IPhone },
+    { id: 4, title: 'Item 2', description: 'Description for Item 2',price:45843,image:IPhone },
+    { id: 5, title: 'Item 3', description: 'Description for Item 3',price:45843,image:IPhone },
   ];
-  return <Box>
-    HELLO THERE TESTED SUCCESSFULLY BY Belay
-    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-    {itemList.map((item)=>(
-      <MediaCard key={item.id} item={item}></MediaCard>
-    ))}
-    </Grid>
+  return <Box sx={{ m: 3 }}>
+    <Box>
+      <Grid container spacing={4}>
+        {itemList.map((item)=>(
+          <MediaCard key={item.id} item={item}></MediaCard>
+        ))}
+      </Grid>
+    </Box>
+    <Box>
+    <div>
+        Grab the best deal on Smartphones
+      </div>
+    </Box>
+    <Box>
+      <Grid container spacing={5}>
+      {phoneItemList.map((item)=>(
+        <PhoneCard key={item.id} item={item}></PhoneCard>
+      ))}
+      </Grid>
+    </Box>
     <Footer></Footer>
   </Box>
 }
