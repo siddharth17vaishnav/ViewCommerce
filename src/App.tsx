5bee79c249554194d373ed6c47827d3be0e8fc72
Ashishvam <ashish@vamverse.com>
2023-07-07 12:07:45 +0530

    update all component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './sections/Home'
-import Footer from './components/Footer/Footer'
+
+
+import Card from './components/card/Card';
+import Footer from './components/Footer/Footer';
+
+
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer/>
-    </>
-  )
+    <div>
+     <Card />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
