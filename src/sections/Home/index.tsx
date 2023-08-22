42a1ce4ca0c433f252ac62e5f54586d0e8c041d8
Ashishvam <ashish@vamverse.com>
2023-08-22 21:31:08 +0530

    added footer, login and signup page

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,41 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CardList from '../../components/card/Card';
 import Footer from '../../components/Footer/Footer';
 import Signin from '../../components/Page/Signin';
 import Login from '../../components/Page/Login';
 import ForgotPassword from '../../components/Page/Forgotpassword';
-import P1 from '../../components/products/P1';
-import LoginCard from '../../components/admin/Logincard';
-import Signincard from '../../components/admin/Signincard';
-import Signindetail from '../../components/admin/Siginindetails';
-import Verifyemail from '../../components/admin/Verifyemail';
-import Businessdetail from '../../components/admin/businessdetail';
-import Congrats from '../../components/admin/Congrats';
-import List1 from '../../components/list/List1';
-import ProductDetail from '../../components/Detail/ProductDetail';
 
 
 const Home = () => {
   return (
     <>
-      <div>
-        <Routes>
-          <Route path="/products/P1" element={<P1 />} />
-        </Routes>
-      </div>
-    {/* <ProductDetail />
-      <List1 />
-      <Congrats />
-      <Businessdetail />
-      <Verifyemail />
-      <Signindetail />
-      <Signincard />
-      <LoginCard />
-      <ForgotPassword />
       <Login />
+      <ForgotPassword />
       <Signin />
-      <CardList /> */}
-      <Footer />
+      <Footer /> 
+      
     </>
   );
 }
